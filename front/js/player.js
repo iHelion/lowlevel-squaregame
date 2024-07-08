9a2a6dac0312586568719d39ccb83eadc0edd756
Helion <148087176+iHelion@users.noreply.github.com>
2024-07-08 23:44:15 +0200

    player cannot move out of canvas and move smooth (#2)
    none

--- front/js/player.js
+++ front/js/player.js
@@ -1,63 +1,49 @@
 const MARGIN = 10
 const SIZE = 50
-const STEP = 20
+const STEP = 5
 
 let X = 0
 let Y = 0
 
-
 function setInitialPosition(ctx) {
-
     const canvas = ctx.canvas
-
     const width = canvas.clientWidth
     const height = canvas.clientHeight
 
     X = (width - SIZE) / 2
     Y = height - SIZE - MARGIN
-
 }
-
 
 export function initialDraw(ctx) {
-
     setInitialPosition(ctx)
     draw(ctx)
-
 }
-
 
 export function draw(ctx) {
-
     ctx.reset()
     ctx.fillRect(X, Y, SIZE, SIZE)
-
 }
-
 
 export function goLeft() {
-
-    X -= STEP
-
+    if (X - STEP >= 0) {
+        X -= STEP
+    }
 }
-
 
 export function goRight() {
-
-    X += STEP
-
+    if (X + STEP + SIZE <= 720) {
+        X += STEP
+    }
 }
-
 
 export function goUp() {
-
-    Y -= STEP
-
+    if (Y - STEP >= 0) {
+        Y -= STEP
+    }
 }
 
-
 export function goDown() {
-
-    Y += STEP
-
+    if (Y + STEP + SIZE <= 720) {
+        Y += STEP
+    }
 }
