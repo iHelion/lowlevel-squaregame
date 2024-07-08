14c03a7f0b4fbced5e072b23916d5073434be009
Helion <sup3rc311.1o1@gmail.com>
2024-07-08 23:39:41 +0200

    player cannot move out of canvas and move smooth
    none

--- front/js/controls.js
+++ front/js/controls.js
@@ -1,59 +1,47 @@
 import {
-
     initialDraw,
     draw,
     goLeft,
     goRight,
     goUp,
     goDown
-
 } from './player.js'
-
 
 const LEFT = 'a'
 const RIGHT = 'd'
 const UP = 'w'
 const DOWN = 's'
 
+let keysPressed = {}
 
-function handleKeyDown(ctx, event) {
-    
-    const key = event.key
-
-    switch (key) {
-
-        case LEFT:
-            goLeft()
-            break
-
-        case RIGHT:
-            goRight()
-            break
-
-        case UP:
-            goUp()
-            break
-
-        case DOWN:
-            goDown()
-            break
-
-    }
-
-    draw(ctx)
-
+function handleKeyDown(event) {
+    keysPressed[event.key] = true
 }
 
+function handleKeyUp(event) {
+    keysPressed[event.key] = false
+}
+
+function update(ctx) {
+    if (keysPressed[LEFT]) {
+        goLeft()
+    }
+    if (keysPressed[RIGHT]) {
+        goRight()
+    }
+    if (keysPressed[UP]) {
+        goUp()
+    }
+    if (keysPressed[DOWN]) {
+        goDown()
+    }
+    draw(ctx)
+    requestAnimationFrame(() => update(ctx))
+}
 
 export function addControls(ctx) {
-
-    document.addEventListener(
-
-        'keydown', 
-        (event) => handleKeyDown(ctx, event)
-    
-    )
-
+    document.addEventListener('keydown', handleKeyDown)
+    document.addEventListener('keyup', handleKeyUp)
     initialDraw(ctx)
-
+    update(ctx)
 }
